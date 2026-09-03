03cd344ffb429b9adf536abe22f6e4ef5ab4bd03
CsSzegleti <szegleti.csongor@mhk.hu>
2026-09-03 08:59:03 +0200

    tweak scratch picker

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -5,25 +5,59 @@ require('snacks').setup {
   image = { enabled = true },
 }
 
+local pickers = require 'telescope.pickers'
 local conf = require('telescope.config').values
-local function toggle_telescope(scratch_files)
-  local file_paths = {}
+local finders = require 'telescope.finders'
+local actions = require 'telescope.actions'
+local action_state = require 'telescope.actions.state'
+
+local function scratch_open_telescope(opts)
+  opts = opts or {}
+  local scratch_files = Snacks.scratch.list()
   for _, item in ipairs(scratch_files) do
-    table.insert(file_paths, item.file)
+    local stat = vim.uv.fs_stat(item.file)
+    -- Fallback to 0 if the file doesn't exist yet on disk
+    item.mtime = stat and stat.mtime.sec or 0
   end
 
-  require('telescope.pickers')
-    .new({}, {
-      prompt_title = 'Scratch Buffers',
-      finder = require('telescope.finders').new_table {
-        results = file_paths,
+  table.sort(scratch_files, function(a, b) return a.mtime > b.mtime end)
+
+  pickers
+    .new(opts, {
+      prompt_title = 'Scratch Files',
+      finder = finders.new_table {
+        results = scratch_files,
+        entry_maker = function(entry)
+          return {
+            value = entry,
+            display = Snacks.picker.util.text(entry, { 'name', 'branch', 'ft' }),
+            ordinal = entry.name .. ' ' .. entry.ft .. ' ' .. entry.file,
+            path = entry.file,
+          }
+        end,
       },
-      previewer = conf.file_previewer {},
-      sorter = conf.generic_sorter {},
+      previewer = conf.file_previewer(opts),
+      sorter = conf.generic_sorter(opts),
+      attach_mappings = function(prompt_bufnr, _)
+        actions.select_default:replace(function()
+          actions.close(prompt_bufnr)
+          local selection = action_state.get_selected_entry()
+          if selection then
+            local item = selection.value
+            Snacks.scratch.open {
+              file = item.file,
+              name = item.name,
+              ft = item.ft,
+              icon = item.icon,
+            }
+          end
+        end)
+        return true
+      end,
     })
     :find()
 end
 
 vim.keymap.set('n', '<leader>.', function() Snacks.scratch() end, { desc = 'Toggle Scratch Buffer' })
 
-vim.keymap.set('n', '<leader>S', function() toggle_telescope(Snacks.scratch.list()) end, { desc = 'Select Scratch Buffer' })
+vim.keymap.set('n', '<leader>S', function() scratch_open_telescope() end, { desc = 'Select Scratch Buffer' })
